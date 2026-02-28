5f1078cb6201e163d82e9b5299b55adda5b267d2
dommii33 <dominikfabry@web.de>
2026-02-28 22:44:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
 
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const TELEGRAM_TOKEN = "DEIN_BOT_TOKEN";
+const TELEGRAM_CHAT_ID = "DEINE_CHAT_ID";
+
+app.post("/webhook", async (req, res) => {
+  const from = req.body.From;
+  const body = req.body.Body;
+
+  await axios.post(
+    `https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`,
+    {
+      chat_id: TELEGRAM_CHAT_ID,
+      text: `Neue WhatsApp Nachricht:\nVon: ${from}\n\n${body}`
+    }
+  );
+
+  res.send("<Response></Response>");
+});
+
+app.listen(3000, () => {
+  console.log("Server läuft auf Port 3000");
+});
